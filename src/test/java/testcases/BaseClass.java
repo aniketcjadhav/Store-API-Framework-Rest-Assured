7e578e9fc91686585549982e12fe6c161363fbdc
Aniket <aniketjadhav241299@gmail.com>
2026-08-11 23:22:54 +0530

    Added Log File Functionality to maintain req, resp logs

--- src/test/java/testcases/BaseClass.java
+++ src/test/java/testcases/BaseClass.java
@@ -1,22 +1,42 @@
 package testcases;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 import org.testng.annotations.BeforeClass;
 
 import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import routes.Routes;
 import utils.ConfigReader;
 
 public class BaseClass {
 	
 	ConfigReader configReader;
+	
+	RequestLoggingFilter requestLoggingFilter;
+	ResponseLoggingFilter responseLoggingFilter;
+
+	
 	@BeforeClass
-	public void setUp()
+	public void setUp() throws FileNotFoundException
 	{
 		RestAssured.baseURI = Routes.Base_URL;
 		
 		configReader = new ConfigReader();
+		
+		// Setup filters for logging
+	    FileOutputStream fos = new FileOutputStream(".\\logs\\test_logging.log");
+	    PrintStream log = new PrintStream(fos, true);
+	    
+	    requestLoggingFilter = new RequestLoggingFilter(log);
+	    responseLoggingFilter = new ResponseLoggingFilter(log);
+	    
+	    RestAssured.filters(requestLoggingFilter, responseLoggingFilter);
+
 	}
 	
 	//Helper Methods
