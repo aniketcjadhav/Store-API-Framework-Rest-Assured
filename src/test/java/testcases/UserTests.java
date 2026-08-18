711ed08776b14fc986f617dcbf77dc43ad14bf7e
Aniket <aniketjadhav241299@gmail.com>
2026-08-18 23:50:12 +0530

    Added Get all User Test

--- src/test/java/testcases/UserTests.java
+++ src/test/java/testcases/UserTests.java
@@ -1,5 +1,31 @@
 package testcases;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+import routes.Routes;
+
 public class UserTests extends BaseClass {
 
+	@Test
+	public void testGetAllUsers()
+	{
+		Response r =
+		given()
+		.when()
+			.get(Routes.GET_ALL_USERS)
+		.then()
+		.statusCode(200)
+		.log().body()
+		.body("size()", greaterThan(0))
+		.extract()
+		.response();
+		
+		int size = r.jsonPath().getInt("size()");
+		System.out.println(size);
+	}
+	
 }
